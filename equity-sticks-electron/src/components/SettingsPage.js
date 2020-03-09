47b1b716d35e3140520b9969462e58e67dfdf5a8
aarizirf <aarizirf@gmail.com>
2020-03-09 09:05:48 -0700

    Added max tallies input on settings page

--- equity-sticks-electron/src/components/SettingsPage.js
+++ equity-sticks-electron/src/components/SettingsPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import propTypes from "prop-types";
-import { Button, Header, Container } from "semantic-ui-react";
+import { Button, Header, Container, Popup, Input } from "semantic-ui-react";
 import Navbar from "./Navbar";
 import Routes from "./routes";
 import DataProvider from "./DataProvider";
@@ -19,12 +19,26 @@ class SettingsPage extends React.Component {
 				<DataProvider.Consumer>
 					{
 						dataCtx => {
-							const {currentClass, classes, preferences, editClass } = dataCtx;
+							const { currentClass, classes, preferences, editClass } = dataCtx;
 							return (
 								<Container>
 									<Header className="mt-md" as="h1">My Settings</Header>
-									<p>Not implemented!</p>
-									<p>On this page you would change global settings such as max tallies, etc</p>
+									<Header as="h4">Max tallies:</Header>
+									<p>Max tallies: 8<span>
+										<Popup on="click" wide="very" trigger={
+											<p>edit</p>
+										}>
+											<Input value="8" labelPosition="right" label={
+												<Button positive icon="save" onClick={() => {
+
+												}} />
+											} onChange={(e, d) => {
+
+											}} />
+										</Popup>
+									</span>
+									</p>
+
 								</Container>
 							);
 						}
